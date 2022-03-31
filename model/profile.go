c244c2bb62107c4b03edc188fd9c3b382e0fdd53
JohnGuoy <johnguoy@163.com>
2022-03-31 22:28:35 +0800

    Add files via upload

--- model/profile.go
+++ model/profile.go
@@ -1,1 +1,20 @@
+package model
 
+type Profile struct {
+	Name              string
+	Gender            string
+	Age               int
+	Height            int
+	Weight            int
+	Income            string // 工资下限和上限
+	Marriage          string
+	Education         string
+	Occupation        string
+	Residence         string // 居住地（工作地）
+	Hukou             string // 籍贯
+	Xinzuo            string
+	House             string
+	Car               string
+	Introduction      string
+	IndividualInfoUrl string
+}
